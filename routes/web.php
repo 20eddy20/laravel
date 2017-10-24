8231ec147c90a331cad7cfac825e5175d6fdf158
20eddy20 <lujan.ecl@gmail.com>
2017-10-24 23:59:12 +0200

    control login

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
-
 
 Route::get('/', 'FrontController@index');
 Route::get('contacto', 'FrontController@contacto');
@@ -24,3 +7,5 @@ Route::get('reviews', 'FrontController@reviews');
 Route::get('admin', 'FrontController@admin');
 
 Route::resource('usuario','UsuarioController');
+Route::resource('log','LogController');
+Route::get('logout', 'LogController@logout');
